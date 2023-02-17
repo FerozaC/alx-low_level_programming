d609869c439274e7e4b14d86eb6ce423eb49d561
Feroza Chishty <chishty452@gmail.com>
2023-02-17 13:57:18 -0800

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
  /**
- * *main - prints "Programming is like building a multiligual puzzle"
- * 
+ * main - prints "Programming is like building a multiligual puzzle"
  * Return: ALways 0 (Success)
  */	
 
