6bd157a67ba272316f182e53d77cbcf7a935f440
Feroza Chishty <chishty452@gmail.com>
2023-02-23 20:03:39 -0800

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
  /**
- * main - prints "Programming is like building a multiligual puzzle"
- * Return: ALways 0 (Success)
- */	
+ * main -  prints "Programming is like building a multiligual puzzle"
+ * Return: ALways 0
+ */
 	int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
