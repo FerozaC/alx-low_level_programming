b3b468b6d4d7d395ff505f3d07e772e92181657f
Feroza Chishty <chishty452@gmail.com>
2023-02-24 00:40:32 -0800

    m

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,12 +10,12 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-/**
-* while (i < 10) - prints an infinite loop
-* {
-* putchar(i);
-* }
-*/
+/*
+ * while (i < 10)
+ * {
+ * putchar(i);
+ * }
+ */
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
