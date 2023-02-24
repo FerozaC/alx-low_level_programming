923865c7a71eea23b960ff0feeaef3e7d270e111
Feroza Chishty <chishty452@gmail.com>
2023-02-23 18:46:10 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_numbers - Prints the numbers from 0 up to 9
+* Return: void
+*/
+void print_numbers(void)
+{
+char c;
+for (c='0',c <='9',c++)
+{
+_putchar(c);
+}
+_putchar('\n);
+}
