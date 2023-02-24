6bd37c2a77239ea30d54995092e4dce9de15b47b
Feroza Chishty <chishty452@gmail.com>
2023-02-24 11:12:08 -0800

    m

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,17 +10,26 @@
 
 void print_diagonal(int n)
 {
-if (n <= 0)
-{
-_putchar('\n');
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			if (i > 1)
+			{
+				for (j = 1; j <= i - 1; j++)
+				{
+				_putchar(' ');
+				}
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
 }
-else
-{
-int i;
-for (i = 1; i <= n; i++)
-{
-_putchar('_');
-}
-_putchar('\n');
-}
-}
+
