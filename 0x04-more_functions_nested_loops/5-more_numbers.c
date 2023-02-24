69114d09195d34e65c0320965d3e04f92325222a
Feroza Chishty <chishty452@gmail.com>
2023-02-24 10:21:33 -0800

    m

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,13 @@ void more_numbers(void)
 {
 
 int i, j;
-for (i = 1; i <= 10; i++)
+for (i = 0; i <= 9; i++)
 {
-for (j = 0; j < 14; j++)
+for (j  =  0; j <=  14; j++)
 {
-if (j >= 10)
-_putchar('1');
-_putchar(j % 10 + '0');
+if (j >= 9)
+_putchar((j / 10) - '0');
+_putchar((j % 10) + '0');
 }
 _putchar('\n');
 }
