0f3915b1e00e3947d271e3b144866c069d4da546
Feroza Chishty <chishty452@gmail.com>
2023-02-27 19:09:37 -0800

    m

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+i#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line
+ * to stdout
+ * @str: string to be processed
+ */
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+{
+	}
+		_putchar('\n');
+}
