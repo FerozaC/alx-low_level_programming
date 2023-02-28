8c69247365ba43b9269b43e697e7e4f132de3299
Feroza Chishty <chishty452@gmail.com>
2023-02-28 10:15:53 -0800

    m

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,7 @@
-i#include "main.h"
-
+#include "main.h"
 /**
- * _puts - prints a string, followed by a new line
- * to stdout
- * @str: string to be processed
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
  */
 void _puts(char *str)
 {
@@ -13,3 +11,4 @@ void _puts(char *str)
 	}
 		_putchar('\n');
 }
+
