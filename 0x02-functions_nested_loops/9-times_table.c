e769e82f88ddb6fe145e8df0347392bd82bc781f
Feroza Chishty <chishty452@gmail.com>
2023-03-06 01:30:02 -0800

    fixing coding style

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,10 +1,10 @@
-#include "main.h"
+include "main.h"
 
 
-	/**
-	 * times_table - prints the 9 times table, starting with 0
-	 */
-	void times_table(void)
+/**
+*times_table -> time table printing
+*/
+void times_table(void)
 	{
 		int i, j, k;
 
