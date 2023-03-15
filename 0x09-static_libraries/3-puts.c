520019c8bf9402bff4e8932e90454bc62b5745b4
Feroza Chishty <chishty452@gmail.com>
2023-03-15 13:24:45 -0700

    m

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,10 +1,15 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
-void _puts(char *s)
+/**
+ * _puts - prints a string
+ * @str: the string
+ * Return: void
+ */
+void _puts(char *str)
 {
-puts(s);
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
 }
+
