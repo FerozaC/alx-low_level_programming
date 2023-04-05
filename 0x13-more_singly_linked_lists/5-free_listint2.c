a2e38c75a0f12283331f36dff2678b5f5ba861e1
Feroza Chishty <chishty452@gmail.com>
2023-04-04 19:39:56 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+* free_listint2 - frees linked list
+* @head: pointer, to listint_t list to be freed
+*/
+void free_listint2(listint_t **head)
+{
+listint_t *tmp;
+if (head == NULL || *head == NULL)
+return;
+
+while (*head)
+{
+tmp = (*head)->next;
+free(*head);
+*head = tmp;
+}
+*head = NULL;
+}
