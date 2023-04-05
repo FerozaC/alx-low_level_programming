a2b43026588fbac52a073a34c0d960723aecaa14
Feroza Chishty <chishty452@gmail.com>
2023-04-05 16:22:47 -0700

    making code kernal style compliant

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,30 +1,25 @@
 #include "lists.h"
-
 /**
- * add_nodeint_end - adds node at the end of linked list
- * @head: pointer to the first element in list
- * @n: data to insert in new element
- * Return: pointer to the new node/NULL if it fails
- */
+* add_nodeint_end - adds node at the end of linked list
+* @head: pointer to the first element in list
+* @n: data to insert in new element
+* Return: pointer to the new node/NULL if it fails
+*/
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new = malloc(sizeof(listint_t));
-    listint_t *tmp = *head;
-    if (!new)
-        return (NULL);
-
-    new->n = n;
-    new->next = NULL;
-
-    if (*head == NULL)
-    {
-        *head = new;
-        return (new);
-    }
-    while (tmp->next)
-        tmp = tmp->next;
-
-    tmp->next = new;
-
-    return (new);
+listint_t *new = malloc(sizeof(listint_t));
+listint_t *tmp = *head;
+if (!new)
+return (NULL);
+new->n = n;
+new->next = NULL;
+if (*head == NULL)
+{
+*head = new;
+return (new);
+}
+while (tmp->next)
+tmp = tmp->next;
+tmp->next = new;
+return (new);
 }
