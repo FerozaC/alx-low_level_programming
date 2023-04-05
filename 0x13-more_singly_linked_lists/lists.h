33eab445ea9b2200ec4c772b88e6907e777d17e1
Feroza Chishty <chishty452@gmail.com>
2023-04-05 16:18:59 -0700

    making code betty compliant

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,15 +4,15 @@
 #include <stdlib.h>
 
 /**
- * Description: singly linked list node structure
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- */
+* Description: singly linked list node structure
+* struct listint_s - singly linked list
+* @n: integer
+* @next: points to the next node
+*/
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
