98be34ab2bce784adaf0a15546cfccf286842355
Feroza Chishty <chishty452@gmail.com>
2023-08-17 19:28:53 -0700

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+* dlistint_len - returns the number of elements in
+* a double linked list
+* @h: head of the list
+* Return: the number of nodes
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t count = 0;
+
+while (h)
+{
+count++;
+if (h->next)
+h = h->next;
+else
+break;
+}
+
+return (count);
+}
+
